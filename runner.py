ec4aa0fde349d5a6b00fe146fda943f017dff9b2
lazlo54 <32623331+lazlo54@users.noreply.github.com>
2019-05-30 23:51:29 +0300

    Update runner.py

--- runner.py
+++ runner.py
@@ -10,25 +10,25 @@ class MyParser:
         letter = plex.Range('azAZ')
         num = plex.Range('09')
         digit = plex.Range('01')
+        andop = plex.Str('and')
+        orop = plex.Str('or')
+        xorop = plex.Str('xor')
         name = letter + plex.Rep(letter|num)
         space = plex.Any(' \n\t')
         Keyword = plex.Str('print','PRINT')
         binary= plex.Rep1(digit)
         equals = plex.Str( '=')
-        andop = plex.Str('&')
-        orop = plex.Str('|')
-        xorop = plex.Str('^')
         parethensys1 = plex.Str('(')
         parethensys2 = plex.Str(')')
         self.varList={}
         self.lexicon = plex.Lexicon([
             (Keyword, 'PRINT_TOKEN'),
-            (name, 'ID_TOKEN'),             #name = letter + plex.Rep(letter|digit)
-            (binary, 'BINARY_NUM'),
-            (equals, '='),
             (andop, plex.TEXT),
             (orop, plex.TEXT),
             (xorop, plex.TEXT),
+            (name, 'ID_TOKEN'),             #name = letter + plex.Rep(letter|digit)
+            (binary, 'BINARY_NUM'),
+            (equals, '='),
             (parethensys1, '('),
             (parethensys2, ')'),
             (space, plex.IGNORE)			
@@ -82,26 +82,26 @@ class MyParser:
     def expr(self):
         if self.la == '(' or self.la == 'ID_TOKEN' or self.la == 'BINARY_NUM' : #first set: ( , id , binary number
             t = self.term()
-            while self.la == '^':
-                self.match('^')
+            while self.la == 'xor':
+                self.match('xor')
                 t2 = self.term()
                 print('xor : {:b} ^ {:b} '.format(t,t2))
                 t = t^t2
             if self.la == ')' or self.la == 'ID_TOKEN' or self.la == 'PRINT_TOKEN' or self.la == None:
                 return t
-            raise ParseError('Expected ^ operator')
+            raise ParseError('Expected xor operator')
         else: #Error
             raise ParseError('Expected a parethensys,an id or a binary number')
             
     def term(self):
         if self.la == '(' or self.la == 'ID_TOKEN' or self.la == 'BINARY_NUM': #first set: (,id,binary number
             f = self.factor()
-            while self.la == '|':
-                self.match('|')
+            while self.la == 'or':
+                self.match('or')
                 f2 = self.factor()
-                print('or : {:b} | {:b} '.format(f,f2))
+                print('or : {:b} or {:b} '.format(f,f2))
                 f = f|f2
-            if self.la == ')' or self.la == '^' or self.la == 'ID_TOKEN' or self.la == 'PRINT_TOKEN' or self.la == None:
+            if self.la == ')' or self.la == 'xor' or self.la == 'ID_TOKEN' or self.la == 'PRINT_TOKEN' or self.la == None:
                 return f
             raise ParseError('Expected | operator')
         else: #Error
@@ -110,12 +110,12 @@ class MyParser:
     def factor(self):
         if self.la == '(' or self.la == 'ID_TOKEN' or self.la == 'BINARY_NUM' : #first set: ( , id , binary number
             a = self.atom()
-            while self.la == '&':
-                self.match('&')
+            while self.la == 'and':
+                self.match('and')
                 a2 = self.atom()
-                print('and : {:b} & {:b} '.format(a,a2))
+                print('and : {:b} and {:b} '.format(a,a2))
                 a = a&a2
-            if self.la == ')' or self.la == '|' or self.la == '^' or self.la == 'ID_TOKEN' or self.la == 'PRINT_TOKEN' or self.la == None:
+            if self.la == ')' or self.la == 'or' or self.la == 'xor' or self.la == 'ID_TOKEN' or self.la == 'PRINT_TOKEN' or self.la == None:
                 return a
             raise ParseError('Expected & operator')
         else: #Error
